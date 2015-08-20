6e5f83dfa4fca6e6fa013422d05a7c53934ac99c
Vyacheslav Moskalenko <smoskalenko@xiag.ch>
2015-08-20 14:51:55 +0600

    Fix issue with adding array element to specific index in array

--- src/Rs/Json/Patch/Operations/Add.php
+++ src/Rs/Json/Patch/Operations/Add.php
@@ -90,7 +90,8 @@ class Add extends Operation
                     $targetArray[] = $this->getValue();
                 } else {
                     if (is_numeric($additionIndex)) {
-                        array_splice($targetArray, $additionIndex, 0, $this->getValue());
+                        $replacement = is_array($this->getValue()) ? array($this->getValue()) : $this->getValue();
+                        array_splice($targetArray, $additionIndex, 0, $replacement);
                     } else {
                         $targetArray[$additionIndex] = $this->getValue();
                     }
